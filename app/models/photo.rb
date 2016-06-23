e5dc6559b4acb924ba7a436867f5e963e1c01964
Dimitry Nazarov <dimitrynazarov@gmail.com>
2016-06-23 01:16:52 -0400

    fixes misspelling

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :gallery
   has_attached_file :image, styles: { thumb: '250x250>', medium: '500x500>', large: '1000x1000>' }
-  validates_attachement_content_type :image, content_type: /Aimage\/.*\Z/
+  validates_attachment_content_type :image, content_type: /Aimage\/.*\Z/
 end
