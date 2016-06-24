7b7574dd5d303959e6e6d160829140fcd6cb450d
Dimitry Nazarov <dimitrynazarov@gmail.com>
2016-06-24 16:50:22 -0400

    refactors PostCategory

--- spec/factories/post_categories.rb
+++ spec/factories/post_categories.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :post_category do
-    
+    category_id { rand(0...11) }
+    post_id     { rand(0...11) }
   end
 end
--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'has a working factory' do
+    expect(create(:post_category)).to be_valid
+  end
+
+  context 'validations' do
+
+    it 'belongs to a category' do
+      expect(PostCategory.reflect_on_association(:category).macro).to eq(:belongs_to)
+    end
+
+    it 'belongs to a post' do
+      expect(PostCategory.reflect_on_association(:post).macro).to eq(:belongs_to)
+    end
+
+    context 'validations' do
+
+      it 'is invalid without a category' do
+        expect{ create(:post_category, category_id: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+
+      it 'is invalid without a post' do
+        expect{ create(:post_category, post_id: nil) }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+
+    end
+
+  end
 end
